b9659aa7b1e90916df6f1ec1d3d00c858f8ef509
nraffa <nraffapirra@gmail.com>
2023-12-19 17:44:32 -0300

    add streamlit frontend

--- frontend/streamlitPrototype.py
+++ frontend/streamlitPrototype.py
@@ -2,6 +2,18 @@ import streamlit as st
 import requests
 
 
+# def get_stream(url, data):
+#     s = requests.Session()
+
+#     with s.post(url, json=data, stream=True) as resp:
+#         for line in resp.iter_lines():
+#             if line:
+#                 return line
+
+
+url = "http://127.0.0.1:8000/chain"
+
+
 st.title("JuriBot")
 
 # Initialize chat history
@@ -13,41 +25,23 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# React to user input
-if prompt := st.chat_input("Talk to me"):
-    # Display user message in chat message container
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    data = {
+        "message": prompt,
+        "chat_history": "",
+    }
+
+    response = requests.post(url, json=data)
+
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-
-    response = f"juri: {prompt}"
-
-    # Display assistant response in chat message container
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
-        full_response = ""
+        full_response = response.text  # get_stream(url, data)
 
-        data = {"message": prompt, "chat_history": ""}
-
-        response = requests.post("http://127.0.0.1:8000/chain", json=data)
-
-        # for response in answer(
-        messages = (
-            [
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-        )
-        stream = (True,)
-
-        full_response += response.text  # .choices[0].delta.content or ""
-        message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
-        st.session_state.messages.append(
-            {"role": "assistant", "content": full_response}
-        )
-
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
