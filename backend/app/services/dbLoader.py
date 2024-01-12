cabae68a6171d47ff90d6e5d561c4089c123d81d
nraffa <nraffapirra@gmail.com>
2024-01-12 13:18:40 -0300

    fix chroma db service name for doc upload

--- backend/app/services/dbLoader.py
+++ backend/app/services/dbLoader.py
@@ -3,6 +3,6 @@ from utils.dbLoaderHelper import vectorStoreLoader
 LOADING_DIRECTORY = input("Do you want to load a directory? (y/n): ")
 
 if LOADING_DIRECTORY == "y":
-    vectorStoreLoader("localhost", 8000, directory=True)
+    vectorStoreLoader("chroma-db", 8000, directory=True)
 else:
-    vectorStoreLoader("localhost", 8000)
+    vectorStoreLoader("chroma-db", 8000)
