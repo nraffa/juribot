91a365bc3cd325725e8a4b11332abe3026682621
nraffa <nraffapirra@gmail.com>
2024-01-21 16:30:59 +0100

    fix importing bug

--- backend/app/services/utils/dbLoaderHelper.py
+++ backend/app/services/utils/dbLoaderHelper.py
@@ -3,7 +3,7 @@
 import chromadb
 from chromadb.config import Settings
 from langchain.vectorstores import Chroma
-from services.utils.documentProcessor import documentProcessor, directoryProcessor
+from .documentProcessor import documentProcessor, directoryProcessor
 from chromadb.utils import embedding_functions
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 import uuid
